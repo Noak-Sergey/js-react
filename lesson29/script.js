81690f20398ce7d6b474aaf601c00d22c3c45840
Noak-Sergey <noakstuff@gmail.com>
2022-04-29 19:14:24 +0300

    ex 35

--- lesson29/script.js
+++ lesson29/script.js
@@ -76,3 +76,98 @@ function writeYourGenres() {
     }
 }
 writeYourGenres();
+
+//==========Упражнение по написанию кода 7
+//Объем и площадь поверхности куба
+function calculateVolumeAndArea(num) {
+    if(!Number.isInteger(num) || num === 'string' || num < 0) {
+        return `При вычислении произошла ошибка`;
+    }
+    const volume = num*num*num;
+    const area = (num*num) * 6;
+    return `Объем куба: ${volume}, площадь всей поверхности: ${area}`
+}
+console.log(calculateVolumeAndArea(5));
+
+// Написать функцию которая будет определять номер купе по переданному ей номеру места
+function getCoupeNumber(seatNumber) {
+    if (typeof(seatNumber) !== 'number' || seatNumber < 0 || !Number.isInteger(seatNumber)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+
+    if (seatNumber === 0 || seatNumber > 36) {
+        return "Таких мест в вагоне не существует";
+    }
+
+    for (let i = 4; i <= 36; i = i + 4) {
+        if (seatNumber <= i) {
+            return Math.ceil(i / 4);
+        }
+    }
+}
+console.log(getCoupeNumber(2));
+
+//======================================================================
+//функция принимает минуты и возвращает в часах
+function getTimeFromMinutes(time) {
+    if (typeof(time) !== 'number' || time < 0 || !Number.isInteger(time)) {
+        return "Ошибка, проверьте данные";
+    }
+    
+    const minute = time % 60;
+    const hours = (time - minute) / 60 ;
+
+    let hoursStr = '';
+
+    switch(hours) {
+        case 0:
+            hoursStr = 'часов';
+            break;
+        case 1: 
+            hoursStr = 'час';
+            break;
+        case 2:    
+        case 3:    
+        case 4:
+            hoursStr = 'часа';
+            break;    
+        default:
+            hoursStr = 'часов';
+    }
+
+    return `Это ${hours} ${hoursStr} и ${minute} минут`
+}
+console.log(getTimeFromMinutes(1260));
+
+// функция принимает 4 числа и возвращает самое большое
+function findMaxNumber(one, two, three, four) {
+    if(typeof(one) !== 'number' || typeof(two) !== 'number' || typeof(three) !== 'number' ||typeof(four) !== 'number') {
+        return 0;
+    }
+    return Math.max(one, two, three, four)
+}
+console.log(findMaxNumber(1, 5, "6", 11));
+
+//задача фибоначи===============
+function fib(n) {
+    if(typeof(n) !== 'number' || n <= 0 || !Number.isInteger(n)) {
+        return '';
+    }
+    let res = '';
+    let one = 0;
+    let two = 1;
+
+    for(let i = 1; i <= n; i++) {
+        if(i === n) {
+            res += `${one}`;
+        } else {
+            res += `${one} `;
+        }
+
+        let cur = one + two;
+        one = two;
+        two = cur;
+    }
+    return res
+}
+console.log(fib(7));
---
+++ lesson32-33-35/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const arr = ['a', 'b', 'c', 'd'];
+const data = {...arr}
+console.log(data);
