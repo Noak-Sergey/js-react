57727d448baa27f099e60b97056ba97360ef83fe
Noak-Sergey <noakstuff@gmail.com>
2022-05-12 22:28:45 +0300

    complete less 44

--- lesson44/project/js/script.js
+++ lesson44/project/js/script.js
@@ -24,3 +24,45 @@ const movieDB = {
     ]
 };
 
+//#1 мой вариант
+//document.querySelector('.promo__adv').remove();
+// const advertising = document.querySelector('.promo__adv');
+// advertising.remove();
+//вариант2 по уроку
+const adv = document.querySelectorAll('.promo__adv img');
+adv.forEach( i => {
+    i.remove();
+});
+
+//#2
+const changeComedy = document.querySelector('.promo__genre');
+changeComedy.textContent = "Драма"
+
+
+//#3
+// const promoBg = document.querySelector('.promo__bg');
+// promoBg.style.cssText  = 'background-image: url(./img/bg.jpg); background-position-x: center; background-position-y: top; background-size: cover;';
+//вариант2 по уроку
+const poster = document.querySelector('.promo__bg');
+poster.style.backgroundImage = 'url(./img/bg.jpg)';
+
+//#4
+// const sortMovies = movieDB.movies.sort((a,b) => {
+//     return a - b;
+// });
+//вариант2 по уроку
+const movieList = document.querySelector('.promo__interactive-list');
+movieList.innerHTML = "";
+movieDB.movies.sort();
+//#5
+// const allMoviesName = document.querySelectorAll('.promo__interactive-item');
+// allMoviesName.forEach( (el, i) => {
+//     el.textContent = `${i + 1}. ${sortMovies[i]}`;
+// });
+//вариант2 по уроку
+movieDB.movies.forEach((film, i) => {
+    movieList.innerHTML += `
+    <li class="promo__interactive-item">${i + 1}. ${film}
+        <div class="delete"></div>
+    </li>`;
+});
