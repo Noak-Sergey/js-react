e429640c1dde2712c3c3726c274a8c8df8578d6f
Noak-Sergey <noakstuff@gmail.com>
2022-05-17 18:23:46 +0300

    less 64

--- lesson63/script.js
+++ lesson63/script.js
@@ -1,19 +1,3 @@
 "use strict";
     const box = document.querySelector('.box');
 
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-        console.log('touchstart');
-        console.log(e.touches);
-    });
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-        console.log('touchmove');
-        console.log(e.targetTouches[0].pageX);
-    });
-
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-        console.log('touchend');
-    });
