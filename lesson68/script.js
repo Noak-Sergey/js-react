59a77bc6e32832c5e5f034146126e3d36adcb76c
Noak-Sergey <noakstuff@gmail.com>
2022-05-19 13:46:41 +0300

    less 69

---
+++ lesson68/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// const now = new Date(); // текущая дата
+// const year = now.getFullYear(); //получаем год текущий
+// const month = now.getMonth(); //текущий месяц
+// const day = now.getDate(); // номер дня
+// const dayWeek = now.getDay(); // день недели
+// const hour = now.getHours(); // текущие час
+// const hourUTC = now.getUTCHours(); // час в UTC
+
+// const div = document.createElement('div');
+// const div1 = document.createElement('div');
+// div.innerHTML = hour;
+// div1.innerHTML = hourUTC;
+
+// document.body.append(div);
+// document.body.append(div1);
+
+
+//Считаем время выполнения операции
+// let start = new Date();
+// for(let i = 0; i < 100000; i++) {
+//     let some = i ** 3;
+// }
+// let end = new Date();
+// alert(`Цикл отработал за ${end - start} миллисекунд`);
