a84ca94360eaffef43a819240dfdced3f9b44284
Noak-Sergey <noakstuff@gmail.com>
2022-06-01 18:07:10 +0300

    less 95 localStorage

---
+++ lesson95/preview/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const checkbox = document.querySelector('#checkbox'),
+    form = document.querySelector('form'),
+    change = document.querySelector('#color');
+
+    if(localStorage.getItem('isChecked')) {
+        checkbox.checked = true;
+    }
+
+    if(localStorage.getItem('bg') === 'changed') {
+        form.style.backgroundColor = 'yellow';
+    }
+    
+    checkbox.addEventListener('change', () => {
+        localStorage.setItem('isChecked', true);
+    });
+
+    change.addEventListener('click', () => {
+        if(localStorage.getItem('bg') === 'changed') {
+            localStorage.removeItem('bg');
+            form.style.backgroundColor = '#fff';
+        } else {
+            localStorage.setItem('bg', 'changed');
+            form.style.backgroundColor = 'yellow';
+        }
+    });
