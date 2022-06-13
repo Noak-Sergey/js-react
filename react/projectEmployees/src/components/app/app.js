189f21fb31fb4a3f908e0d831324c919d84a8cff
Noak-Sergey <noakstuff@gmail.com>
2022-06-13 21:53:06 +0300

    less 132

--- react/projectEmployees/src/components/app/app.js
+++ react/projectEmployees/src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'John C.' , salary: 800, increase: false, id: 1},
+        {name: 'Alex B.' , salary: 1200, increase: true, id: 2},
+        {name: 'Carl W.' , salary: 3000, increase: false, id: 3},
+    ];
+
     return (
         <div className="app">
             <AppInfo/>
@@ -17,7 +24,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     );
