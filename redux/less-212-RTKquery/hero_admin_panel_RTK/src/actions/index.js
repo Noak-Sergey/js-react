f0e41855a142832037d6881be0772f80ee33b55c
Noak-Sergey <noakstuff@gmail.com>
2022-09-14 10:06:33 +0300

    less 209

---
+++ redux/less-212-RTKquery/hero_admin_panel_RTK/src/actions/index.js
@@ -0,0 +1,9 @@
+//Пример с задержкой
+// export const activeFilterChanged = (filter) => (dispatch) => {
+//     setTimeout(() => {
+//         dispatch({
+//             type: 'ACTIVE_FILTER_CHANGED',
+//             payload: filter
+//         })
+//     }, 1000)
+// }
